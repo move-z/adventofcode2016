c80e69a5270355cfce7ae076a92ea14575c67188
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-10-20 16:55:19 +0200

    day 2

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -11,7 +11,17 @@ object Day02 {
     res.toString()
   }
 
-  def second(input: String): String = ???
+  def second(input: String): String = {
+    val pad = Array[Seq[Option[String]]](Array(None, None, Some("1"), None, None), Array(None, Some("2"), Some("3"), Some("4"), None), Array(Some("5"), Some("6"), Some("7"), Some("8"), Some("9")), Array(None, Some("A"), Some("B"), Some("C"), None), Array(None, None, Some("D"), None, None))
+    val start = Pad(pad, 0, 2)
+    var res = new StringBuilder
+    input.split("\n").foldLeft(start)((p, line) => {
+      val next = advance(line, p)
+      res ++= next.element
+      next
+    })
+    res.toString()
+  }
 
   def advance(input: String, pad: Pad): Pad = {
     if (input.length > 0) {
@@ -83,39 +93,13 @@ object Day02 {
         |RRDDD
         |LURDL
         |UUUUD""".stripMargin, "1985")
-    test(first, lines, "48584")
+    println(first(lines))
 
     test(second,
       """ULL
         |RRDDD
         |LURDL
         |UUUUD""".stripMargin, "5DB3")
-    test(second, lines, "563B6")
+    println(second(lines))
   }
 }
-
-
-//You finally arrive at the bathroom (it's a several minute walk from the lobby so visitors can behold the many fancy
-// conference rooms and water coolers on this floor) and go to punch in the code. Much to your bladder's dismay, the
-// keypad is not at all like you imagined it. Instead, you are confronted with the result of hundreds of man-hours of
-// bathroom-keypad-design meetings:
-//
-//    1
-//  2 3 4
-//5 6 7 8 9
-//  A B C
-//    D
-//
-//You still start at "5" and stop when you're at an edge, but given the same instructions as above, the outcome is very
-// different:
-//
-//    You start at "5" and don't move at all (up and left are both edges), ending at 5.
-//    Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"), ending at D.
-//    Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
-//    Finally, after five more moves, you end at 3.
-//
-//So, given the actual keypad layout, the code would be 5DB3.
-//
-//Using the same instructions in your puzzle input, what is the correct bathroom code?
-//
-//Your puzzle answer was 563B6.
