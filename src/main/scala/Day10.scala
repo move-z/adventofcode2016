d0f60496e87f396e8362fe078210dd0049bb82a8
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-10-23 15:07:00 +0200

    day 10

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -6,16 +6,24 @@ object Day10 {
     val bots = mutable.Map[Int, Bot]()
     val outputs = mutable.Map[Int, Int]()
 
-    def find(): Option[Int] = {
-      bots.mapValues(_.values).filter(_._2.isDefined).find(b => b._2.get._1 == 17 && b._2.get._2 == 61).map(_._1)
-    }
+    def find(): Option[Int] = bots.mapValues(_.values).filter(_._2.isDefined).find(b => b._2.get._1 == 17 && b._2.get._2 == 61).map(_._1)
 
     init(bots, instructions)
     run(bots, outputs, instructions, _ => find().isDefined)
     find().get
   }
 
-  def second(input: String): Int = ???
+  def second(input: String): Int = {
+    val instructions = input.split("\n")
+    val bots = mutable.Map[Int, Bot]()
+    val outputs = mutable.Map[Int, Int]()
+
+    def find(): Boolean = outputs.contains(0) && outputs.contains(1) && outputs.contains(2)
+
+    init(bots, instructions)
+    run(bots, outputs, instructions, _ => find())
+    outputs(0) * outputs(1) * outputs(2)
+  }
 
   lazy private val init = "value (\\d+) goes to bot (\\d+)".r
 
@@ -114,35 +122,7 @@ object Day10 {
     run(bots, outputs, instructions, _ => outputs.contains(0) && outputs.contains(1) && outputs.contains(2))
 
     println(first(lines))
-    test(first, lines, 27)
 
-//    test(first, , 27)
+    println(second(lines))
   }
 }
-
-
-//value 5 goes to bot 2
-//bot 2 gives low to bot 1 and high to bot 0
-//value 3 goes to bot 1
-//bot 1 gives low to output 1 and high to bot 0
-//bot 0 gives low to output 2 and high to output 0
-//value 2 goes to bot 2
-//
-//    Initially, bot 1 starts with a value-3 chip, and bot 2 starts with a value-2 chip and a value-5 chip.
-//    Because bot 2 has two microchips, it gives its lower one (2) to bot 1 and its higher one (5) to bot 0.
-//    Then, bot 1 has two microchips; it puts the value-2 chip in output 1 and gives the value-3 chip to bot 0.
-//    Finally, bot 0 has two microchips; it puts the 3 in output 2 and the 5 in output 0.
-//
-//In the end, output bin 0 contains a value-5 microchip, output bin 1 contains a value-2 microchip, and output bin 2
-// contains a value-3 microchip. In this configuration, bot number 2 is responsible for comparing value-5 microchips
-// with value-2 microchips.
-//
-//Based on your instructions, what is the number of the bot that is responsible for comparing value-61 microchips with
-// value-17 microchips?
-//
-//Your puzzle answer was 27.
-//--- Part Two ---
-//
-//What do you get if you multiply together the values of one chip in each of outputs 0, 1, and 2?
-//
-//Your puzzle answer was 13727.
