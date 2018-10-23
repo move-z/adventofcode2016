35a82f751aa7c3d8c29b96ab297bbd7264e7edbc
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-10-23 15:04:56 +0200

    day 10

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,34 +1,63 @@
+import scala.collection.mutable
+
 object Day10 {
-  def first(input: String): Int = ???
+  def first(input: String): Int = {
+    val instructions = input.split("\n")
+    val bots = mutable.Map[Int, Bot]()
+    val outputs = mutable.Map[Int, Int]()
+
+    def find(): Option[Int] = {
+      bots.mapValues(_.values).filter(_._2.isDefined).find(b => b._2.get._1 == 17 && b._2.get._2 == 61).map(_._1)
+    }
+
+    init(bots, instructions)
+    run(bots, outputs, instructions, _ => find().isDefined)
+    find().get
+  }
 
   def second(input: String): Int = ???
 
   lazy private val init = "value (\\d+) goes to bot (\\d+)".r
-  def init(map: Map[Int, Bot], instructions: Seq[String]): Unit = {
-    instructions.foreach { case init(v, bot) => addValue(map, bot.toInt, v.toInt) }
+
+  def init(map: mutable.Map[Int, Bot], instructions: Seq[String]): Unit = {
+    instructions.foreach {
+      case init(v, bot) => addValue(map, bot.toInt, v.toInt)
+      case _ =>
+    }
   }
 
   lazy private val move = "bot (\\d+) gives low to (bot|output) (\\d+) and high to (bot|output) (\\d+)".r
-  def run(map: Map[Int, Bot], outputs: Map[Int, Int], instructions: Seq[String], stopExec: Map[Int, Bot] => Boolean = _ => true): Unit = {
+
+  def run(map: mutable.Map[Int, Bot], outputs: mutable.Map[Int, Int], instructions: Seq[String], stopExec: mutable.Map[Int, Bot] => Boolean = _ => true): Unit = {
+    var running = true
     do {
-      instructions.foreach {
-        case move(from, lowType, lowVal, highType, highVal) =>
-          val values = getValues(map, from.toInt)
-          if (values.isDefined) {
-            if (lowType == "bot")
-              addValue(map, lowVal.toInt, values.get._1)
-            else
-              outputs(lowVal.toInt) = values.get._1
-            if (highType == "bot")
-              addValue(map, highVal.toInt, values.get._2)
-            else
-              outputs(highVal.toInt) = values.get._2
-          }
-      }
-    } while (!stopExec(map))
+      running = false
+      instructions.foreach(running ||= exec(map, outputs, _))
+    } while (!stopExec(map) && running)
   }
 
-  def addValue(map: Map[Int, Bot], bot: Int, value: Int): Unit = {
+  def exec(map: mutable.Map[Int, Bot], outputs: mutable.Map[Int, Int], instruction: String): Boolean = {
+    instruction match {
+      case move(from, lowType, lowVal, highType, highVal) =>
+        val values = getValues(map, from.toInt)
+        if (values.isDefined) {
+          if (lowType == "bot")
+            addValue(map, lowVal.toInt, values.get._1)
+          else
+            outputs(lowVal.toInt) = values.get._1
+          if (highType == "bot")
+            addValue(map, highVal.toInt, values.get._2)
+          else
+            outputs(highVal.toInt) = values.get._2
+          true
+        } else {
+          false
+        }
+      case _ => false
+    }
+  }
+
+  def addValue(map: mutable.Map[Int, Bot], bot: Int, value: Int): Unit = {
     if (map.contains(bot)) {
       map(bot) += value
     } else {
@@ -36,11 +65,15 @@ object Day10 {
     }
   }
 
-  def getValues(map: Map[Int, Bot], bot: Int): Option[(Int, Int)] = {
-    val res = map(bot).values
-    if (res.isDefined)
-      map(bot) = new Bot()
-    res
+  def getValues(map: mutable.Map[Int, Bot], bot: Int): Option[(Int, Int)] = {
+    if (!map.contains(bot))
+      None
+    else {
+      val res = map(bot).values
+      if (res.isDefined)
+        map(bot) = new Bot()
+      res
+    }
   }
 
   class Bot (val a: Option[Int] = None, val b: Option[Int] = None) {
@@ -67,7 +100,23 @@ object Day10 {
     val lines = try source.mkString finally source.close()
 
     def test[T] = (fun: String => T, input: String, expected: T) => assert(fun(input) == expected, input)
-    test(first, lines, true)
+
+    val instructions =
+      """value 5 goes to bot 2
+        |bot 2 gives low to bot 1 and high to bot 0
+        |value 3 goes to bot 1
+        |bot 1 gives low to output 1 and high to bot 0
+        |bot 0 gives low to output 2 and high to output 0
+        |value 2 goes to bot 2""".stripMargin.split("\n")
+    val bots = mutable.Map[Int, Bot]()
+    val outputs = mutable.Map[Int, Int]()
+    init(bots, instructions)
+    run(bots, outputs, instructions, _ => outputs.contains(0) && outputs.contains(1) && outputs.contains(2))
+
+    println(first(lines))
+    test(first, lines, 27)
+
+//    test(first, , 27)
   }
 }
 
