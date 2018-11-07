30e1afed24ac2813a77bf60d1452bac3ad974703
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-11-07 23:26:34 +0100

    day 21

--- src/main/scala/Day21.scala
+++ src/main/scala/Day21.scala
@@ -1,71 +1,86 @@
 object Day21 {
-  def first(input: String): Long = ???
+  def first(input: String): String = {
+    val arr = "abcdefgh".toCharArray
+    input.split("\n").foreach(apply(arr, _))
+    arr.mkString
+  }
 
-  def second(input: String): Int = ???
+  def second(input: String): String = ???
+
+  private val swapP = "swap position (\\d+) with position (\\d+)".r
+  private val swapL = "swap letter (.) with letter (.)".r
+  private val rotateLeft = "rotate left (\\d+) steps?".r
+  private val rotateRight = "rotate right (\\d+) steps?".r
+  private val rotateP = "rotate based on position of letter (.)".r
+  private val reverse = "reverse positions (\\d+) through (\\d+)".r
+  private val move = "move position (\\d+) to position (\\d+)".r
+
+  def apply(arr: Array[Char], instruction: String): Unit = {
+    def _swapP(a: Int, b: Int): Unit = {
+      val c = arr(a)
+      arr(a) = arr(b)
+      arr(b) = c
+    }
+    def _swapL(a: Char, b: Char): Unit = {
+      val ai = arr.indexOf(a)
+      val bi = arr.indexOf(b)
+      val c = arr(ai)
+      arr(ai) = arr(bi)
+      arr(bi) = c
+    }
+    def _rotateLeft(n: Int): Unit = {
+      val _n = if (n > arr.length) n % arr.length else n
+      val cpy = arr.clone()
+      val split = cpy.splitAt(_n)
+      split._2.indices.foreach(i => arr(i) = split._2(i))
+      split._1.indices.foreach(i => arr(i + arr.length - _n) = split._1(i))
+    }
+    def _rotateRight(n: Int): Unit = {
+      val _n = if (n > arr.length) n % arr.length else n
+      val cpy = arr.clone()
+      val split = cpy.splitAt(arr.length - _n)
+      split._1.indices.foreach(i => arr(i + _n) = split._1(i))
+      split._2.indices.foreach(i => arr(i) = split._2(i))
+    }
+    def _rotateP(l: Char): Unit = {
+      val idx = arr.indexOf(l) + 1
+      val n = if (idx > 4) idx + 1 else idx
+      _rotateRight(n)
+    }
+    def _reverse(from: Int, to: Int): Unit = {
+      val r = (to to from by -1).map(arr(_))
+      r.indices.foreach(i => arr(i + from) = r(i))
+    }
+    def _move(from: Int, to: Int): Unit = {
+      val c = arr(from)
+      if (from < to)
+        (from until to).foreach(i => arr(i) = arr(i + 1))
+      else
+        (from until to by -1).foreach(i => arr(i) = arr(i - 1))
+      arr(to) = c
+    }
+
+    instruction match {
+      case swapP(a, b) => _swapP(a.toInt, b.toInt)
+      case swapL(a, b) => _swapL(a.head, b.head)
+      case rotateLeft(n) => _rotateLeft(n.toInt)
+      case rotateRight(n) => _rotateRight(n.toInt)
+      case rotateP(l) => _rotateP(l.head)
+      case reverse(from, to) => _reverse(from.toInt, to.toInt)
+      case move(from, to) => _move(from.toInt, to.toInt)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     val source = io.Source.fromFile("src/main/resources/day21.txt")
     val lines = try source.mkString finally source.close()
 
-    def test[T] = (fun: String => T, input: String, expected: T) => assert(fun(input) == expected, input)
-
-    test(first, lines, "bfheacgd")
-
     println(first(lines))
 
-    println(second(lines))
+//    println(second(lines))
   }
 }
 
-//The computer system you're breaking into uses a weird scrambling function to store its passwords. It shouldn't be much
-// trouble to create your own scrambled password so you can add it to the system; you just have to implement the
-// scrambler.
-//
-//The scrambling function is a series of operations (the exact list is provided in your puzzle input). Starting with the
-// password to be scrambled, apply each operation in succession to the string. The individual operations behave as
-// follows:
-//
-//    swap position X with position Y means that the letters at indexes X and Y (counting from 0) should be swapped.
-//    swap letter X with letter Y means that the letters X and Y should be swapped (regardless of where they appear in
-// the string).
-//    rotate left/right X steps means that the whole string should be rotated; for example, one right rotation would
-// turn abcd into dabc.
-//    rotate based on position of letter X means that the whole string should be rotated to the right based on the index
-// of letter X (counting from 0) as determined before this instruction does any rotations. Once the index is determined,
-// rotate the string to the right one time, plus a number of times equal to that index, plus one additional time if the
-// index was at least 4.
-//    reverse positions X through Y means that the span of letters at indexes X through Y (including the letters at X
-// and Y) should be reversed in order.
-//    move position X to position Y means that the letter which is at index X should be removed from the string, then
-// inserted such that it ends up at index Y.
-//
-//For example, suppose you start with abcde and perform the following operations:
-//
-//    swap position 4 with position 0 swaps the first and last letters, producing the input for the next step, ebcda.
-//    swap letter d with letter b swaps the positions of d and b: edcba.
-//    reverse positions 0 through 4 causes the entire string to be reversed, producing abcde.
-//    rotate left 1 step shifts all letters left one position, causing the first letter to wrap to the end of the string:
-// bcdea.
-//    move position 1 to position 4 removes the letter at position 1 (c), then inserts it at position 4 (the end of the
-// string): bdeac.
-//    move position 3 to position 0 removes the letter at position 3 (a), then inserts it at position 0 (the front of
-// the string): abdec.
-//    rotate based on position of letter b finds the index of letter b (1), then rotates the string right once plus a
-// number of times equal to that index (2): ecabd.
-//    rotate based on position of letter d finds the index of letter d (4), then rotates the string right once, plus a
-// number of times equal to that index, plus an additional time because the index was at least 4, for a total of 6 right
-// rotations: decab.
-//
-//After these steps, the resulting scrambled password is decab.
-//
-//Now, you just need to generate a new scrambled password and you can access the system. Given the list of scrambling
-// operations in your puzzle input, what is the result of scrambling abcdefgh?
-//
-//Your puzzle answer was bfheacgd.
-//
-//The first half of this puzzle is complete! It provides one gold star: *
-//--- Part Two ---
-//
 //You scrambled the password correctly, but you discover that you can't actually modify the password file on the system.
 // You'll need to un-scramble one of the existing passwords by reversing the scrambling process.
 //
