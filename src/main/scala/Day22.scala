7fad85198ea1174e7c71a3d66dc17cef3f11f29a
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-11-09 19:06:19 +0100

    day 22

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -1,3 +1,7 @@
+import java.lang.Math.abs
+
+import scala.annotation.tailrec
+
 object Day22 {
   def first(input: String): Int = {
     val nodes = input.split("\n").map(parse).filter(_.isDefined).map(_.get)
@@ -5,21 +9,54 @@ object Day22 {
     nodes.combinations(2).flatMap(_.permutations).count(c => viable(c(0), c(1)))
   }
 
-  def second(input: String): Int = ???
+  def second(input: String): Int = {
+    val nodes = input.split("\n").map(parse).filter(_.isDefined).map(_.get)
 
-  case class Node(name: String, size: Int, used: Int)
+    val empty = nodes.find(_.used == 0).get
+    val (top, stepsToTop) = rise(empty, nodes)
+
+    val width = nodes.maxBy(_.x).x
+    val stepsToData = width - top.y - 1
+
+    val loopSize = width * 2
+    val stepsToTgt = (loopSize - 1) * (width - 1) + 1
+
+    stepsToTop + stepsToData + stepsToTgt
+  }
+
+  case class Node(x: Int, y: Int, size: Int, used: Int)
 
   def viable(from: Node, to: Node): Boolean = from.used > 0 && to.used + from.used <= to.size
 
-  private val reg = "(.+)\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T.*".r
+  def rise(from: Node, nodes: Seq[Node]): (Node, Int) = {
+    @tailrec def _r(f: Node, acc: Int = 0): (Node, Int) = {
+      if (f.y == 0)
+        (f, acc)
+      else {
+        val next = find(f.size, f.x, nodes.filter(_.y == f.y - 1))
+        _r(next, acc + 1 + abs(next.x - f.x))
+      }
+    }
+    _r(from)
+  }
+
+  def find(space: Int, fromX: Int, nodes: Seq[Node]): Node = {
+    def fit(n: Node) = space <= n.size
+    if (fit(nodes(fromX)))
+      nodes(fromX)
+    else
+      nodes.filter(fit).minBy(s => abs(s.x - fromX))
+  }
+
+  private val reg = "/dev/grid/node-x(\\d+)-y(\\d+)\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T.*".r
   def parse(in: String): Option[Node] =
     in match {
-      case reg(name, s, u, a) =>
+      case reg(x, y, s, u, a) =>
         val size = s.toInt
         val used = u.toInt
         val avail = a.toInt
         assert(used + avail == size)
-        Some(Node(name, size, used))
+        Some(Node(x.toInt, y.toInt, size, used))
       case _ => None
     }
 
@@ -28,5 +65,7 @@ object Day22 {
     val lines = try source.mkString finally source.close()
 
     println(first(lines))
+
+    println(second(lines))
   }
 }
