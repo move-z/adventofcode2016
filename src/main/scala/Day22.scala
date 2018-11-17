0a0066de446afcdac59fea55c5404198ecf52db7
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-11-17 11:30:00 +0100

    day 22

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -1,65 +1,52 @@
+import java.lang.Math.abs
+
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 object Day22 {
   def first(input: String): Int = {
     val nodes = input.split("\n").map(parse).filter(_.isDefined).map(_.get)
-
-    def viable(from: Node, to: Node): Boolean = from.used > 0 && to.used + from.used <= to.size
 
     nodes.combinations(2).flatMap(_.permutations).count(c => viable(c(0), c(1)))
   }
 
   def second(input: String): Int = {
     val nodes = input.split("\n").map(parse).filter(_.isDefined).map(_.get)
-    val cluster = Cluster(nodes.map(n => (n.x, n.y) -> n).toMap)
 
-    val seen = mutable.Set[Cluster]()
+    val empty = nodes.find(_.used == 0).get
+    val (top, stepsToTop) = rise(empty, nodes)
 
-    @tailrec def find(in: Seq[Cluster], steps: Int = 0): Int = {
-      println(steps)
-      println(in.length)
-      if (in.exists(_.tgt == (0, 0)))
-        steps
-      else
-        find(in.flatMap(_.next).filter(seen.add), steps + 1)
-    }
+    val width = nodes.maxBy(_.x).x + 1
+    val stepsToData = width - top.x - 2
 
-    find(Seq(cluster))
-  }
+    val loopSize = 5
+    val stepsToTgt = loopSize * (width - 2) + 1
 
-  case class Cluster(nodes: Map[(Int, Int), Node], tgt: (Int, Int) = (34, 0)) {
-    val empty: Node = nodes.values.find(_.used == 0).get
-
-    def next: Seq[Cluster] = {
-      def opt(bool: Boolean) = if (bool) Some(true) else None
-
-      def fits(x: Int, y: Int) = {
-        val n = nodes(x, y)
-        if (empty.size < n.used) None else Some(n)
-      }
-
-      def move(from: Node, to: Node): Cluster = {
-        val f = Node(from.x, from.y, from.size, 0)
-        val t = Node(to.x, to.y, to.size, from.used)
-        val newTgt = if (from.x == tgt._1 && from.y == tgt._2) (to.x, to.y) else tgt
-
-        Cluster(nodes + ((f.x, f.y) -> f) + ((t.x, t.y) -> t), newTgt)
-      }
-
-      val r =
-      opt(empty.x > 0).flatMap(_ => fits(empty.x - 1, empty.y)).map(move(_, empty)) +:
-      opt(empty.x < 34).flatMap(_ => fits(empty.x + 1, empty.y)).map(move(_, empty)) +:
-      opt(empty.y > 0).flatMap(_ => fits(empty.x, empty.y - 1)).map(move(_, empty)) +:
-//      opt(empty.x < 34).flatMap(_ => fits(empty.x, empty.y + 1)).map(move(_, empty)) +:
-      Nil
-
-      r.filter(_.isDefined).map(_.get)
-    }
-
+    stepsToTop + stepsToData + stepsToTgt
   }
 
   case class Node(x: Int, y: Int, size: Int, used: Int)
+
+  def viable(from: Node, to: Node): Boolean = from.used > 0 && to.used + from.used <= to.size
+
+  def rise(from: Node, nodes: Seq[Node]): (Node, Int) = {
+    @tailrec def _r(f: Node, acc: Int = 0): (Node, Int) = {
+      if (f.y == 0)
+        (f, acc)
+      else {
+        val next = find(f.size, f.x, nodes.filter(_.y == f.y - 1))
+        _r(next, acc + 1 + abs(next.x - f.x))
+      }
+    }
+    _r(from)
+  }
+
+  def find(space: Int, fromX: Int, nodes: Seq[Node]): Node = {
+    def fit(n: Node) = space >= n.used
+    if (fit(nodes(fromX)))
+      nodes(fromX)
+    else
+      nodes.filter(fit).minBy(s => abs(s.x - fromX))
+  }
 
   private val reg = "/dev/grid/node-x(\\d+)-y(\\d+)\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T.*".r
   def parse(in: String): Option[Node] =
