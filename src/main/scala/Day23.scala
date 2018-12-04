8468feef7a9531994cc5d83a879b201ec62ce9f4
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-12-04 13:51:05 +0100

    day 23

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -2,13 +2,14 @@ import scala.annotation.tailrec
 
 object Day23 {
   def first(input: String): Int = {
-    val instructions = optimize(input.split("\n").map(Instruction.parse))
+    val instructions = input.split("\n").map(Instruction.parse)
     val state = Machine(instructions, Map(Reg("a") -> 7))
 
     @tailrec def run(machine: Machine): Machine = {
-      machine.current match {
+      val next = machine.advance()
+      next match {
         case None => machine
-        case Some(i) => run(i.apply(machine).move(1))
+        case Some(i) => run(i)
       }
     }
 
@@ -16,37 +17,34 @@ object Day23 {
   }
 
   def second(input: String): Int = {
-    val instructions = optimize(input.split("\n").map(Instruction.parse))
+    val instructions = input.split("\n").map(Instruction.parse)
     val state = Machine(instructions, Map(Reg("a") -> 12))
 
     @tailrec def run(machine: Machine): Machine = {
-      println(f"ip: ${machine.ip} regs: ${machine.regs}")
-      machine.current match {
+      val next = machine.advance()
+      next match {
         case None => machine
-        case Some(i) => run(i.apply(machine).move(1))
+        case Some(i) => run(i)
       }
     }
 
     run(state)(Reg("a"))
   }
 
-  def optimize(instructions: Seq[Instruction]): Seq[Instruction] = {
-    case class AddPlaceHolder(index: Int, from: Reg, to: Reg)
+  def optimize(instructions: Seq[Instruction]): (Int, Instruction) = {
+    val opt5: PartialFunction[Seq[Instruction], Instruction] = {
+      case Seq(Inc(to), Dec(from1), Jnz(cmp1, Value(-2)), Dec(from2), Jnz(cmp2, Value(-5))) if from1 == cmp1 && from2 == cmp2 => Mul(from1, from2, to)
+      case Seq(Inc(to), Inc(from1), Jnz(cmp1, Value(-2)), Inc(from2), Jnz(cmp2, Value(-5))) if from1 == cmp1 && from2 == cmp2 => Mul(from1, from2, to)
+    }
 
-    val newInstruction = (0 until instructions.length - 2).map(i => (instructions(i), instructions(i + 1), instructions(i + 2)) match {
-      case (Inc(to), Dec(from), Jnz(cmp, Value(-2))) if from == cmp => Some(AddPlaceHolder(i, from, to))
-      case (Dec(from), Inc(to), Jnz(cmp, Value(-2))) if from == cmp => Some(AddPlaceHolder(i, from, to))
-      case _ => None
-    }).find(_.isDefined).flatten
+    val opt3: PartialFunction[Seq[Instruction], Instruction] = {
+      case Seq(Inc(to), Dec(from), Jnz(cmp, Value(-2))) if from == cmp => Add(from, to)
+      case Seq(Dec(from), Inc(to), Jnz(cmp, Value(-2))) if from == cmp => Add(from, to)
+    }
 
-    if (newInstruction.isEmpty)
-      return instructions
-
-    val add = newInstruction.get
-
-    val head = instructions.slice(0, add.index)
-    val tail = instructions.slice(add.index + 3, instructions.length)
-    head ++ Seq(Add(add.from, add.to), Nop(), Nop()) ++ optimize(tail)
+    val try5 = instructions.slice(0, 5)
+    val try3 = instructions.slice(0, 3)
+    if (opt5.isDefinedAt(try5)) (5, opt5(try5)) else if (opt3.isDefinedAt(try3)) (3, opt3(try3)) else (1, instructions.head)
   }
 
   case class Machine(instructions: Seq[Instruction], regs: Map[Reg, Int] = Map(), ip: Int = 0) {
@@ -58,7 +56,12 @@ object Day23 {
 
     def move(offset: Int): Machine = Machine(instructions, _regs, ip + offset)
 
-    def current: Option[Instruction] = if (ip >= 0 && ip < instructions.length) Some(instructions(ip)) else None
+    def advance(): Option[Machine] = {
+      if (ip < 0 || ip >= instructions.length) None else {
+        val subst = optimize(instructions.slice(ip, instructions.length))
+        Some(subst._2.apply(this).move(subst._1))
+      }
+    }
 
     override def toString: String = s"[$ip] $regs"
   }
@@ -150,6 +153,10 @@ object Day23 {
 
   case class Nop() extends Instruction {
     override def apply(state: Machine): Machine = state
+  }
+
+  case class Mul(from1: Reg, from2: Reg, to: Reg) extends Instruction {
+    override def apply(state: Machine): Machine = state.updated(to, to.value(state) + from1.value(state) * from2.value(state)).updated(from1, 0).updated(from2, 0)
   }
 
   def main(args: Array[String]): Unit = {
